1710b2d1a8161a270a63fb329b2653ed396dda15
Robert Nelson <robertcnelson@gmail.com>
2017-06-08 14:05:06 -0500

    ipumm-fw_3.00.12.00-r2: extract
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ipumm-fw/3.00.12.00-r2/extract.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+if [ -d ./ignore ] ; then
+	rm -rf ./ignore || true
+fi
+
+if [ ! -d ./files ] ; then
+	mkdir -p files
+fi
+
+mkdir -p ./ignore
+
+cd ./ignore/
+ar xv ../ipumm-fw_3.00.12.00-r2_armv7ahf-neon.ipk
+tar xf data.tar.gz
+cp lib/firmware/dra7-ipu2-fw.xem4.ipumm-fw ../files/dra7-ipu2-fw.xem4
+cp lib/firmware/dra7-ipu2-fw.xem4.map ../files/
+cd ../
+rm -rf ./ignore || true
+
