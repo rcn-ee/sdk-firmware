3a9eef0f17be5d9b053788d5562f03ff6ef82cee
Robert Nelson <robertcnelson@gmail.com>
2020-03-12 15:47:45 -0500

    opencl-monitor-ipu: 1.2.00.02-r0.0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ opencl-monitor-ipu/1.2.00.02-r0.0/extract.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+if [ -d ./ignore ] ; then
+	rm -rf ./ignore || true
+fi
+
+if [ ! -d ./files ] ; then
+	mkdir -p files
+fi
+
+mkdir -p ./ignore
+
+cd ./ignore/
+ar xv ../opencl-monitor-ipu_*_am57xx_evm.ipk
+tar xf data.tar.xz
+cp -v lib/firmware/dra7-ipu1-fw.xem4.opencl-monitor ../files/dra7-ipu1-fw.xem4
+ls -lha ./lib/firmware/
+cd ../
+rm -rf ./ignore || true
