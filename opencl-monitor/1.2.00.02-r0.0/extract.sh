7976deccc9e6f4b11cdb6b446ad9dcc19795c1e3
Robert Nelson <robertcnelson@gmail.com>
2020-03-12 14:57:30 -0500

    opencl-monitor: add 1.2.00.02-r0.0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ opencl-monitor/1.2.00.02-r0.0/extract.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+if [ -d ./ignore ] ; then
+	rm -rf ./ignore || true
+fi
+
+if [ ! -d ./files ] ; then
+	mkdir -p files
+fi
+
+mkdir -p ./ignore
+
+cd ./ignore/
+ar xv ../opencl-monitor_*_am57xx_evm.ipk
+tar xf data.tar.xz
+cp -v lib/firmware/dra7-dsp1-fw.xe66.opencl-monitor ../files/dra7-dsp1-fw.xe66
+cp -v lib/firmware/dra7-dsp2-fw.xe66.opencl-monitor ../files/dra7-dsp2-fw.xe66
+ls -lha ./lib/firmware/
+cp -v usr/share/ti/opencl/dsp_syms.obj ../files/dsp_syms.obj
+cp -v usr/share/ti/opencl/dsp.out ../files/dsp.out
+cp -v usr/share/ti/opencl/dsp.syms ../files/dsp.syms
+ls -lha ./usr/share/ti/opencl/
+cd ../
+rm -rf ./ignore || true
+
