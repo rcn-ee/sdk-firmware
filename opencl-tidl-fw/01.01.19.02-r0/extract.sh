99c151dfe1aa76dc02a93bb064e28bbccd3ced57
Robert Nelson <robertcnelson@gmail.com>
2020-03-23 15:24:00 -0500

    opencl-tidl-fw: 01.01.19.02-r0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ opencl-tidl-fw/01.01.19.02-r0/extract.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+if [ -d ./ignore ] ; then
+	rm -rf ./ignore || true
+fi
+
+if [ ! -d ./files ] ; then
+	mkdir -p files
+fi
+
+mkdir -p ./ignore
+
+cd ./ignore/
+ar xv ../opencl-tidl-fw_*_am57xx_evm.ipk
+tar xf data.tar.xz
+cp usr/share/ti/ti-opencl-tidl-fw-tree/eve_firmware.bin ../files/eve_firmware.bin
+cp usr/share/ti/ti-opencl-tidl-fw-tree/ocl_tidl_dsp.lib ../files/ocl_tidl_dsp.lib
+cd ../
+rm -rf ./ignore || true
+
